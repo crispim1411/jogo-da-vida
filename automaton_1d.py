581d0015ecf061ff48cea3fbb387e69d473dd380
Crispim <crispim1411@gmail.com>
2021-08-16 23:38:27 -0300

    estrutura generica para automatos 1d

--- automaton_1d.py
+++ automaton_1d.py
@@ -2,38 +2,39 @@ import numpy as np
 from numpy.random import rand
 import matplotlib.pyplot as plt
 
-# Automato Rule 18
-# s(t+1,x) = s(t,x-1) xor s(t,x) xor (s(t,x+1))
+N = 100 # tamanho do vetor
+STEPS = 50
 
-tv = 100 # tamanho do vetor
-steps = 100
-def update():
-    # import pdb; pdb.set_trace()
-    a = np.zeros(tv) # estado atual das células
-    new_a = np.zeros(tv) # novo estado das células
-    a = np.around(rand(tv)) # vetor de 0 e 1
-    # a = np.array([0,0,0,0,0,1,0,0,0,0])
+def update(vector, rule):
+    new_values = np.zeros(N)
+    for i in range(N):
+        p,q,r = vector[(i-1) % N], vector[i], vector[(i+1) % N]
+        if rule(p,q,r):
+            new_values[i] = 1
+    return new_values
 
-    B = np.zeros((steps, tv))
-    B[0] = a.copy()
-    g = 1 # geração atual
-    while g < steps:
-        # import pdb; pdb.set_trace()
-        new_a = np.zeros(tv)
-        for i in range(0, tv):
-            p,q,r = a[(i-1) % tv], a[i], a[(i+1) % tv]
-            if (p,q,r) in [(1,0,0), (0,0,1)]:
-                new_value = 1
-            else:
-                new_value = 0
-            new_a[i] = new_value
-        a = new_a.copy()
-        B[g] = a.copy()
-        g += 1
-    return B
+def run (rule, title):
+    initial_vector = np.zeros(N)
+    initial_vector[N//2] = 1
+    B = np.zeros((STEPS, N))
+    B[0] = initial_vector.copy()
+    values = initial_vector
+    for i in range(1, STEPS):
+        new_values = update(values, rule)
+        B[i] = new_values
+        values = new_values
 
-m = update()
-plt.imshow(m)
+    plt.figure()
+    plt.title(title)
+    plt.imshow(B)
+    
+rule18 = lambda p,q,r: True if (p, q, r) in [(1,0,0), (0,0,1)] else False
+run(rule18, 'Rule 18')
+
+rule90 = lambda p,q,r: True if p != r else False
+run(rule90, 'Rule 90')
+
+rule150 = lambda p,q,r: True if (p,q,r).count(1) in [3, 1] else False
+run(rule150, 'Rule 150')
+
 plt.show()
-
-
